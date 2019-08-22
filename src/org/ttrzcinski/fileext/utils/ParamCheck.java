0721e5a0f1b2ea55d268e78c26127c4c84d08042
Tomasz Trzciński <trzcinski.tomasz.1988@gmail.com>
2019-08-22 13:33:34 +0200

    Update ParamCheck.java
    Added JavaDoc to util.

--- src/org/ttrzcinski/fileext/utils/ParamCheck.java
+++ src/org/ttrzcinski/fileext/utils/ParamCheck.java
@@ -2,25 +2,54 @@ package org.ttrzcinski.fileext.utils;
 
 import java.nio.file.InvalidPathException;
 import java.nio.file.Paths;
+import java.util.Set;
 
-public class ParamCheck {
+/**
+ * Checks given parameters.
+ */
+public final class ParamCheck {
 
+    /**
+     * Known unix paths, which are OK.
+     */
+    private static final Set<String> FIXED_PATHS = Set.of("~", ".");
+
+    /**
+     * Hidden constructor - there is no point to initialize an instance.
+     */
+    private ParamCheck() { }
+
+    /**
+     * Checks, if given path is a valid file path.
+     *
+     * @param path given path
+     * @return true means it's valid, false otherwise
+     */
     public static boolean isPath(final String path) {
-        if (!isSet(path)) return false;
+        boolean result = false;
+        if (!isSet(path)) { return result; }
         final String fixedPath = path.trim().toLowerCase();
         // Check, if it is a home or root
-        if (fixedPath.equals("~") || fixedPath.equals(".")) {
-            return true;
+        if (FIXED_PATHS.contains(fixedPath)) {
+            result = true;
+        } else {
+            try {
+                Paths.get(fixedPath);
+                result = true;
+            } catch (InvalidPathException | NullPointerException ex) {
+                result = false;
+            }
         }
-        try {
-            Paths.get(fixedPath);
-        } catch (InvalidPathException | NullPointerException ex) {
-            return false;
-        }
-        return true;
+        return result;
     }
 
-    public static boolean isSet(String given) {
+    /**
+     * Checks, if given parameter is set meaning has value.
+     *
+     * @param given given parameter
+     * @return true means it has value, false otherwise
+     */
+    public static boolean isSet(final String given) {
         return given != null && given.trim().length() > 2;
     }
 }

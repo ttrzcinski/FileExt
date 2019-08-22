169cd447cf3565c2bf20c113692ee11be2c7cb01
Tomasz Trzciński <trzcinski.tomasz.1988@gmail.com>
2019-08-22 13:29:40 +0200

    Create package-info.java
    Added package-info for utils.

---
+++ src/org/ttrzcinski/fileext/utils/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Set of utils classes with externalized methods for common use elsewhere.
+ * <p>
+ * Contains:<br/>
+ * - FileExt - File path operations<br/>
+ * - JSONOutput - methods to serialize instance to JSON<br/>
+ * - ParamCheck - methods to check enter params<br/>
+ * - StringFix - methods to fix strings to common formats
+ *
+ * @since 1.0
+ * @author <a href="mailto:trzcinski.tomasz.1988@gmail.com">Tomasz T.</a>
+ * @version 1.0
+ */
+package org.ttrzcinski.fileext.utils;

18bcf38bd20571f26147eb9a72bdfaef4e5c75c7
Tomasz Trzciński <trzcinski.tomasz.1988@gmail.com>
2019-08-22 13:32:54 +0200

    Update StringFix.java
    Added JavaDoc comments to util.

--- src/org/ttrzcinski/fileext/utils/StringFix.java
+++ src/org/ttrzcinski/fileext/utils/StringFix.java
@@ -1,12 +1,36 @@
 package org.ttrzcinski.fileext.utils;
 
-public class StringFix {
+/**
+ * Keeps methods to fix String to common standard.
+ */
+public final class StringFix {
 
-    public static String toNotNull(String given) {
-        return given == null || given.trim().length() == 0 ? "" : given.trim();
+    /**
+     * Hidden constructor - there is no point to initialize an instance.
+     */
+    private StringFix() { }
+
+    /**
+     * Fixed string to not-null initialized empty string, if is null.
+     *
+     * @param given given string to fix
+     * @return fixed string
+     */
+    public static String toNotNull(final String given) {
+        if (given == null || given.trim().length() == 0) {
+            return "";
+        } else {
+            return given.trim();
+        }
     }
 
-    public static String simple(String given) {
+    /**
+     * Simplifies string to trimmed and lowercase form.
+     *
+     * @param given given string to fix
+     * @return fixed string
+     */
+    public static String simple(final String given) {
         return toNotNull(given).toLowerCase();
     }
 }

a7592bc00ada16e986125d527d3392358ad503be
Tomasz Trzciński <trzcinski.tomasz.1988@gmail.com>
2019-08-22 13:30:50 +0200

    Update I2JSON.java
    Added JavaDoc comments.

--- src/org/ttrzcinski/fileext/interfaces/I2JSON.java
+++ src/org/ttrzcinski/fileext/interfaces/I2JSON.java
@@ -1,6 +1,14 @@
 package org.ttrzcinski.fileext.interfaces;
 
+/**
+ * Forces adding method to convert instance to JSON string.
+ */
 public interface I2JSON {
 
+    /**
+     * Converts this instance to JSON string.
+     *
+     * @return JSON string
+     */
     String toJSON();
 }

9f3579ecaae9f49fbb67e5cac30e484d886dd75d
Tomasz_Trzcinski <Tomasz_Trzcinski>
2019-08-25 15:53:37 +0200

    Simplified Mounts. Changed Mount to Singleton. Removed builder from Mount. Remvoed usage of builder.

--- src/org/ttrzcinski/fileext/Mount.java
+++ src/org/ttrzcinski/fileext/Mount.java
@@ -8,53 +8,149 @@ import java.io.File;
 import java.nio.file.Path;
 import java.util.List;
 
+/**
+ * Represents a single known mounted point of directories structure.
+ */
 public class Mount implements IPath {
 
-    private Path path;
+    /**
+     * Kept path of the directory.
+     */
+    private Path keptPath;
 
+    /**
+     * Hidden constructor - there is no point to initialize an instance.
+     */
     private Mount() { }
 
-    public Mount(@NotNull String path) {
-        this.path = Path.of(path);
+    /**
+     * Creates new instance with given path.
+     *
+     * @param path given path
+     */
+    private Mount(@NotNull final Path path) {
+        this.keptPath = path;
     }
 
-    @Override
-    public Path getPath() {
-        return this.path;
+    /**
+     * Creates new instance with given path.
+     *
+     * @param path given path
+     * @return an instance
+     */
+    public static Mount of(@NotNull final Path path) {
+        return new Mount(path);
     }
 
-    @Override
-    public List<File> getFiles() {
-        return FileExt.listFilesOf(this.path);
+    /**
+     * Creates new instance with given path.
+     *
+     * @param path given path
+     * @return new instance
+     */
+    public static Mount of(@NotNull final String path) {
+        return new Mount(Path.of(path));
     }
 
-    @Override
-    public List<File> getFiles(final String filter) {
-        return FileExt.listFilesOf(this.path, filter);
+    /**
+     * Creates new instance with given path with subdirectory.
+     *
+     * @param path given path
+     * @param subdirectory subdirectory under given path
+     * @return new instance
+     */
+    public static Mount of(@NotNull final String path, @NotNull final  String subdirectory) {
+        return new Mount(Path.of(String.format("%s/%s", path, subdirectory)));
     }
 
+    /**
+     * Returns kept path.
+     *
+     * @return kept path
+     */
     @Override
-    public List<File> getSubdirectories() {
-        return FileExt.listSubdirectoriesOf(this.path);
+    public final Path getPath() {
+        return this.keptPath;
     }
 
+    /**
+     * Returns list of files within this mount.
+     *
+     * @return list of files within this path
+     */
     @Override
-    public List<File> getSubdirectories(String filter) {
-        return FileExt.listSubdirectoriesOf(this.path, filter);
+    public final List<File> getFiles() {
+        return FileExt.listFilesOf(this.keptPath);
     }
 
+    /**
+     * Returns list of files within this mount filtered by passed filter.
+     *
+     * @param filter given filter criteria
+     * @return list of files, if found, empty list otherwise
+     */
     @Override
-    public File create() {
-        return FileExt.create(this.path);
+    public final List<File> getFiles(final String filter) {
+        return FileExt.listFilesOf(this.keptPath, filter);
     }
 
+    /**
+     * Returns list of subdirectories within mount.
+     *
+     * @return list of subdirectories, if found, empty list otherwise
+     */
     @Override
-    public File remove() {
-        return FileExt.remove(this.path);
+    public final List<File> getSubdirectories() {
+        return FileExt.listSubdirectoriesOf(this.keptPath);
     }
 
+    /**
+     * Returns list of subdirectories within mount with filter.
+     *
+     * @param filter given filter criteria
+     * @return list of subdirectories, if found, empty list otherwise
+     */
     @Override
-    public boolean exists() {
-        return FileExt.exists(this.path);
+    public final List<File> getSubdirectories(final String filter) {
+        return FileExt.listSubdirectoriesOf(this.keptPath, filter);
+    }
+
+    /**
+     * Creates file following kept path.
+     *
+     * @return handle to the file
+     */
+    @Override
+    public final File create() {
+        return FileExt.create(this.keptPath);
+    }
+
+    /**
+     * Returns handle to file pointed by path.
+     *
+     * @return handle to file
+     */
+    public final File read() {
+        return FileExt.read(this.keptPath);
+    }
+
+    /**
+     * Removes file kept in the path.
+     *
+     * @return handle to the file
+     */
+    @Override
+    public final File remove() {
+        return FileExt.remove(this.keptPath);
+    }
+
+    /**
+     * Checks, if file exists in kept path.
+     *
+     * @return true means it exists, false otherwise
+     */
+    @Override
+    public final boolean exists() {
+        return FileExt.exists(this.keptPath);
     }
 }
--- src/org/ttrzcinski/fileext/Mounts.java
+++ src/org/ttrzcinski/fileext/Mounts.java
@@ -1,9 +1,10 @@
 package org.ttrzcinski.fileext;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONObject;
+import org.ttrzcinski.fileext.interfaces.I2Console;
 import org.ttrzcinski.fileext.interfaces.I2JSON;
-import org.ttrzcinski.fileext.utils.JSONOutput;
-import org.ttrzcinski.fileext.utils.ParamCheck;
+import org.ttrzcinski.fileext.interfaces.IMounting;
 import org.ttrzcinski.fileext.utils.StringFix;
 
 import java.nio.file.Path;
@@ -12,90 +13,165 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class Mounts implements I2JSON {
+/**
+ * Keeps all known mounted paths and methods to operate on the collection.
+ */
+public class Mounts implements I2JSON, I2Console, IMounting<Mount> {
 
-    private static Map<String, Path> mounts;
+    /**
+     * Saved mounts of named paths.
+     */
+    private static Map<String, Mount> mounts;
 
-    public Mounts() {
-        initPaths();
+    /**
+     * Kept only instance of mounts set.
+     */
+    private static Mounts instance;
 
+    /**
+     * Crates new mounts set.
+     */
+    private Mounts() {
         readBasePaths();
     }
 
-    private static void initPaths() {
-        if (mounts == null) {
-            mounts = new HashMap<>();
+    /**
+     * Obtains the only existing instance of Mounts.
+     *
+     * @return kept instance
+     */
+    public static Mounts getInstance() {
+        if (instance == null) {
+            // synchronized block to remove overhead
+            synchronized (Mounts.class) {
+                // if instance is null, initialize
+                if (instance == null) {
+                    mounts = new HashMap<>();
+                    instance = new Mounts();
+                }
+            }
         }
+        return instance;
     }
 
-    public void readBasePaths() {
+    /**
+     * Reads basic paths from current project.
+     */
+    public static void readBasePaths() {
         // Read project's path
         Path currentRelativePath = Paths.get("");
         String projectDir = currentRelativePath.toAbsolutePath().toString();
-        mounts.put("project", Path.of(projectDir));
+        mounts.put("project", Mount.of(projectDir));
         // It's sources
-        mounts.put("sources", Path.of(String.format("%s/src", projectDir)));
+        mounts.put("sources", Mount.of(projectDir, "src"));
         // It's tests
-        mounts.put("tests", Path.of(String.format("%s/test", projectDir)));
+        mounts.put("tests", Mount.of(projectDir, "test"));
         // It's resources
-        mounts.put("resources", Path.of(String.format("%s/res", projectDir)));
+        mounts.put("resources", Mount.of(projectDir, "res"));
         // It's outputs
-        mounts.put("out", Path.of(String.format("%s/out", projectDir)));
+        mounts.put("out", Mount.of(projectDir, "out"));
         // User's home directory
         String home = System.getProperty("user.home");
-        mounts.put("home", Path.of(home));
+        mounts.put("home", Mount.of(home));
     }
 
+    /**
+     * Checks, if mounts contains given name.
+     *
+     * @param name given name
+     * @return true means it exists, false otherwise
+     */
+    public static final boolean contains(@NotNull final String name) {
+        String fixed = StringFix.simple(name);
+        return fixed.length() > 0 ? mounts.containsKey(fixed) : false;
+    }
 
-    // Static class Builder
-    public static class MountsBuilder {
-
-        /// instance fields
-        private Map<String, Path> mounts;
-
-        public MountsBuilder() {
-            this.mounts = new HashMap<>();
-        }
-
-        // Setter methods
-        public MountsBuilder mount(String name, Path path) {
-            if (ParamCheck.isSet(name) && path != null) {
-                mounts.put(name.toLowerCase(), path);
-            }
-            return this;
-        }
-
-        public MountsBuilder mount(String name, String path) {
-            if (ParamCheck.isSet(name) && ParamCheck.isPath(path)) {
-                mounts.put(name.toLowerCase(), Path.of(path));
-            }
-            return this;
-        }
-
-        public MountsBuilder unmount(String name) {
-            if (ParamCheck.isSet(name)) {
-                mounts.remove(name.toLowerCase());
-            }
-            return this;
-        }
-
-        // build method to deal with outer class
-        // to return outer instance
-        public Mounts build() {
-            var instance = new Mounts();
-            mounts.keySet().forEach(key -> instance.mount(key, mounts.get(key)));
-            return instance;
+    /**
+     * Removes all kept mounts,
+     */
+    public void clear() {
+        if (mounts != null) {
+            mounts.clear();
         }
     }
 
     /**
-     * Returns pointed mount's path, if mount exists.
+     * Mounts given path with given name.
      *
-     * @param name name of mount
-     * @return mount's path, if found, null otherwise
+     * @param name given name
+     * @param path mount's path
+     * @return handle to the mounts
      */
-    public Path getPathOf(String name) {
-        return ParamCheck.isSet(name) ? mounts.get(name.trim().toLowerCase()) : null;
+    public Mounts withMount(@NotNull final String name, @NotNull final Path path) {
+        this.mount(name, path);
+        return instance;
+    }
+
+    /**
+     * Mounts given path with given name.
+     *
+     * @param name given name
+     * @param path mount's path
+     * @return handle to the mounts
+     */
+    public Mounts withMount(@NotNull final String name, @NotNull final String path) {
+        this.mount(name, path);
+        return instance;
+    }
+
+    /**
+     * Mounts given path with given name.
+     * *
+     *
+     * @param name given name
+     * @param mount mount's path
+     */
+    @Override
+    public void mount(@NotNull final String name,  @NotNull final Mount mount) {
+        String fixed = StringFix.simple(name);
+        if (fixed.length() > 0 && mount != null) {
+            mounts.put(name.toLowerCase(), mount);
+        }
+    }
+
+    /**
+     * Mounts given path with given name.
+     * *
+     *
+     * @param name given name
+     * @param path mount's path
+     */
+    public void mount(@NotNull final String name, @NotNull final Path path) {
+        String fixed = StringFix.simple(name);
+        if (fixed.length() > 0 && path != null) {
+            mounts.put(name.toLowerCase(), Mount.of(path));
+        }
+    }
+
+    /**
+     * Mounts given path with given name.
+     *
+     * @param name given name
+     * @param path mount's path
+     */
+    public void mount(@NotNull final String name, @NotNull final String path) {
+        String fixed = StringFix.simple(name);
+        String fixedPath = StringFix.toNotNull(path);
+        if (fixed.length() > 0 && fixedPath.length() > 0) {
+            mounts.put(fixed, Mount.of(fixedPath));
+        }
+    }
+
+    /**
+     * Removes mount with given name.
+     *
+     * @param name given name of mount
+     */
+    public void unmount(@NotNull final String name) {
+        String fixed = StringFix.simple(name);
+        if (contains(fixed)) {
+            mounts.remove(fixed);
+        }
     }
 
     /**
@@ -104,38 +180,34 @@ public class Mounts implements I2JSON {
      * @param name name of mount
      * @return mount, if found, null otherwise
      */
-    public Mount get(String name) {
-        return ParamCheck.isSet(name) ? new Mount(StringFix.simple(name)) : null;
+    public static final Mount get(String name) {
+        String fixed = StringFix.simple(name);
+        return contains(fixed) ? mounts.get(fixed) : null;
     }
 
-    public void toConsole() {
-        initPaths();
+    public static final Map<String, Mount> getAll() {
+        return mounts.entrySet().stream()
+                .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
+    }
 
+    /**
+     * Shows on console
+     */
+    public void toConsole() {
         System.out.println("List of mounts:");
         mounts.forEach((key, value) -> System.out.printf("%s : %s%n", key, value));
     }
 
-    public void mount(String name, Path path) {
-        if (ParamCheck.isSet(name) && path != null) {
-            mounts.put(name.toLowerCase(), path);
-        }
-    }
-
-    public void mount(String name, String path) {
-        if (ParamCheck.isSet(name) && ParamCheck.isPath(path)) {
-            mounts.put(name.toLowerCase(), Path.of(path));
-        }
-    }
-
-    public void unmount(String name) {
-        if (ParamCheck.isSet(name)) {
-            mounts.remove(name.toLowerCase());
-        }
-    }
-
-    public String toJSON() {
-        Map<String,String> newMap = mounts.entrySet().stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().toAbsolutePath().toString()));
+    /**
+     * Returns set of kept Mounts as JSON set.
+     *
+     * @return JSON set with all kept mounts.
+     */
+    public final String toJSON() {
+        Map<String, String> newMap = mounts.entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey, e -> e.getValue()
+                                .getPath().toAbsolutePath().toString()));
         return new JSONObject(newMap).toString();
     }
 }

1e54fa486a6d0ef9df42723429c587807e4d4943
ttrzcinski <trzcinski.tomasz.1988@gmail.com>
2019-08-26 23:07:18 +0200

    Covered more methods in StringFix with Unit Tests,

--- test/org/ttrzcinski/fileext/utils/StringFixTest.java
+++ test/org/ttrzcinski/fileext/utils/StringFixTest.java
@@ -66,4 +66,147 @@ class StringFixTest {
         // Assert
         assertEquals(testObject, result);
     }
+
+    @Test
+    void simple_someValue() {
+        // Arrange
+        String testObject = "  TEST  ";
+        String expected = "test";
+
+        // Act
+        String result = StringFix.simple(testObject);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void simple_empty() {
+        // Arrange
+        String testObject = "    ";
+        String expected = "";
+
+        // Act
+        String result = StringFix.simple(testObject);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void simple_null() {
+        // Arrange
+        String testObject = null;
+        String expected = "";
+
+        // Act
+        String result = StringFix.simple(testObject);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void padRight_someValue() {
+        // Arrange
+        String testObject = "test";
+        String expected = "test      ";
+
+        // Act
+        String result = StringFix.padRight(testObject, 10);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void padRight_tooSmallValue() {
+        // Arrange
+        String testObject = "test";
+        String expected = "test";
+
+        // Act
+        String result = StringFix.padRight(testObject, 3);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void padRight_zeroValue() {
+        // Arrange
+        String testObject = "test";
+        String expected = "test";
+
+        // Act
+        String result = StringFix.padRight(testObject, 0);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void padRight_negativeValue() {
+        // Arrange
+        String testObject = "test";
+        String expected = "test";
+
+        // Act
+        String result = StringFix.padRight(testObject, -1);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void padLeft_someValue() {
+        // Arrange
+        String testObject = "test";
+        String expected = "      test";
+
+        // Act
+        String result = StringFix.padLeft(testObject, 10);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void padLeft_tooSmallValue() {
+        // Arrange
+        String testObject = "test";
+        String expected = "test";
+
+        // Act
+        String result = StringFix.padLeft(testObject, 3);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void padLeft_zeroValue() {
+        // Arrange
+        String testObject = "test";
+        String expected = "test";
+
+        // Act
+        String result = StringFix.padLeft(testObject, 0);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void padLeft_negativeValue() {
+        // Arrange
+        String testObject = "test";
+        String expected = "test";
+
+        // Act
+        String result = StringFix.padLeft(testObject, -1);
+
+        // Assert
+        assertEquals(expected, result);
+    }
 }
